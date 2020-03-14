605c6b8a90111400a6282150e06beb7e4748dbcd
suraj7yadav <surajyadav0149@gmail.com>
2020-03-14 21:18:52 +0530

    commited

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
-from flask import Flask, render_template, url_for, request, redirect
+from flask import Flask, render_template, url_for, request, redirect, flash
 from pytube import YouTube
-import re
+import re,os
 
 app = Flask(__name__)
 
@@ -15,7 +15,9 @@ def download():
     try:
         yt = YouTube(link)
         vid = yt.streams.filter(progressive=True).first()
-        vid.download()
+        fold = os.path.join(os.getenv('USERPROFILE'), 'Downloads')
+        vid.download(fold)
+        flash("Downloaded successfully")
     except:
         error=2
     if(not error):
